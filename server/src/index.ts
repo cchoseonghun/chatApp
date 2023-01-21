1e226d1580464bf985115c2a2dc16310832675f1
Marcin Bugaj <mbugaj93@gmail.com>
2023-01-21 01:41:55 +0100

    fix: cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,7 +39,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatapp-90k1.onrender.com/",
     credentials: true,
   },
 });
